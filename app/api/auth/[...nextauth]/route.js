ced7da59c794266d9a242c36749443adcfcbf34c
nguynguy <0412.nguyen@gmail.com>
2025-12-09 00:31:51 +0700

    fixbug-auth-v1

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,7 +12,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
